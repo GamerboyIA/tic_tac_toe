10c1dc36aa9204dc70c7d4bc5c4b3713ef08302b
isaac akins <isaac@ideadrop.com>
2024-10-24 12:40:00 -0400

    10/24 12:39

---
+++ uttt.js
@@ -0,0 +1,153 @@
+var turn = 'X';
+
+function update_turn() {
+  if (turn == 'X') {
+    turn = 'O';
+  } else {
+    turn = 'X';
+   }
+   document.getElementById("turn").innerHTML = turn;
+}
+
+// -----Row 1----- //
+
+// Cell 1 //
+
+function r1c1 () {
+  document.getElementById("r1c1").innerhtml = turn;
+  update_turn();
+}
+
+function r1c2 () {
+  document.getElementById("r1c2").innerhtml = turn;
+  update_turn();
+}
+
+function r1c3 () {
+  document.getElementById("r1c3").innerhtml = turn;
+  update_turn();
+}
+
+// Cell 2 //
+
+function r1c4 () {
+  document.getElementById("r1c4").innerhtml = turn;
+  update_turn();
+}
+
+function r1c5 () {
+  document.getElementById("r1c5").innerhtml = turn;
+  update_turn();
+}
+
+function r1c6 () {
+  document.getElementById("r1c6").innerhtml = turn;
+  update_turn();
+}
+
+// Cell 3 //
+
+function r1c7 () {
+  document.getElementById("r1c7").innerhtml = turn;
+  update_turn();
+}
+
+function r1c8 () {
+  document.getElementById("r1c8").innerhtml = turn;
+  update_turn();
+}
+
+function r1c9 () {
+  document.getElementById("r1c9").innerhtml = turn;
+  update_turn();
+}
+
+// -----/Row 1----- //
+// -----Row 2----- //
+
+// Cell 1 //
+
+function r1c1 () {
+
+}
+
+function r1c2 () {
+
+}
+
+function r1c3 () {
+
+}
+
+// Cell 2 //
+
+function r1c4 () {
+
+}
+
+function r1c5 () {
+
+}
+
+function r1c6 () {
+
+}
+
+// Cell 3 //
+
+function r1c7 () {
+
+}
+
+function r1c8 () {
+
+}
+
+function r1c9 () {
+}
+
+// -----/Row 2----- // 
+// -----Row 3----- //
+
+// Cell 1 //
+
+function r1c1 () {
+
+}
+
+function r1c2 () {
+
+}
+
+function r1c3 () {
+
+}
+// Cell 2 //
+
+function r1c4 () {
+
+}
+
+function r1c5 () {
+
+}
+
+function r1c6 () {
+
+}
+
+// Cell 3 //
+
+function r1c7 () {
+
+}
+
+function r1c8 () {
+
+}
+
+function r1c9 () {
+
+}
+
+// -----/Row 3----- //
