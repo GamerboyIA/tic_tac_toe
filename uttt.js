a7bfba6f0ef1cfaf354d46cb4dc48a503419022d
isaac akins <isaac@ideadrop.com>
2024-10-25 09:45:50 -0400

    10/25 9:45

--- uttt.js
+++ uttt.js
@@ -9,9 +9,9 @@ function update_turn() {
    document.getElementById("turn").innerHTML = turn;
 }
 
-// -----Row 1----- //
+// -----CELL 1----- //
 
-// Cell 1 //
+// ROW 1 //
 
 function r1c1 () {
   document.getElementById("r1c1").innerHTML = turn;
@@ -28,44 +28,7 @@ function r1c3 () {
   update_turn();
 }
 
-// Cell 2 //
-
-function r1c4 () {
-  document.getElementById("r1c4").innerHTML = turn;
-  update_turn();
-}
-
-function r1c5 () {
-  document.getElementById("r1c5").innerHTML = turn;
-  update_turn();
-}
-
-function r1c6 () {
-  document.getElementById("r1c6").innerHTML = turn;
-  update_turn();
-}
-
-// Cell 3 //
-
-function r1c7 () {
-  document.getElementById("r1c7").innerHTML = turn;
-  update_turn();
-}
-
-function r1c8 () {
-  document.getElementById("r1c8").innerHTML = turn;
-  update_turn();
-}
-
-function r1c9 () {
-  document.getElementById("r1c9").innerHTML = turn;
-  update_turn();
-}
-
-// -----/Row 1----- //
-// -----Row 2----- //
-
-// Cell 1 //
+// ROW 2 //
 
 function r2c1 () {
   document.getElementById("r2c1").innerHTML = turn;
@@ -82,7 +45,45 @@ function r2c3 () {
   update_turn();
 }
 
-// Cell 2 //
+// ROW 3 //
+
+function r3c1 () {
+  document.getElementById("r3c1").innerHTML = turn;
+  update_turn();
+}
+
+function r3c2 () {  
+  document.getElementById("r3c2").innerHTML = turn;
+  update_turn();
+}
+
+function r3c3 () {
+  document.getElementById("r3c3").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 1 -----//
+//----- CELL 2 -----//
+
+// ROW 1 //
+
+function r1c4 () {
+  document.getElementById("r1c4").innerHTML = turn;
+  update_turn();
+}
+
+function r1c5 () {
+  document.getElementById("r1c5").innerHTML = turn;
+  update_turn();
+}
+
+function r1c6 () {
+  document.getElementById("r1c6").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 2 //
+
 
 function r2c4 () {
   document.getElementById("r2c4").innerHTML = turn;
@@ -99,43 +100,7 @@ function r2c6 () {
   update_turn();
 }
 
-// Cell 3 //
-
-function r2c7 () {
-  document.getElementById("r2c7").innerHTML = turn;
-  update_turn();
-}
-
-function r2c8 () {
-  document.getElementById("r2c8").innerHTML = turn;
-  update_turn();
-}
-
-function r2c9 () {
-  document.getElementById("r2c9").innerHTML = turn;
-  update_turn();
-}
-
-// -----/Row 2----- // 
-// -----Row 3----- //
-
-// Cell 1 //
-
-function r3c1 () {
-  document.getElementById("r3c1").innerHTML = turn;
-  update_turn();
-}
-
-function r3c2 () {
-  document.getElementById("r3c2").innerHTML = turn;
-  update_turn();
-}
-
-function r3c3 () {
-  document.getElementById("r3c3").innerHTML = turn;
-  update_turn();
-}
-// Cell 2 //
+// ROW 3 //
 
 function r3c4 () {
   document.getElementById("r3c4").innerHTML = turn;
@@ -152,7 +117,44 @@ function r3c6 () {
   update_turn();
 }
 
-// Cell 3 //
+//----- /CELL 2 -----//
+//----- CELL 3 -----//
+
+// ROW 1 //
+
+function r1c7 () {
+  document.getElementById("r1c7").innerHTML = turn;
+  update_turn();
+}
+
+function r1c8 () {
+  document.getElementById("r1c8").innerHTML = turn;
+  update_turn();
+}
+
+function r1c9 () {
+  document.getElementById("r1c9").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 2 //
+
+function r2c7 () {
+  document.getElementById("r2c7").innerHTML = turn;
+  update_turn();
+}
+
+function r2c8 () {
+  document.getElementById("r2c8").innerHTML = turn;
+  update_turn();
+}
+
+function r2c9 () {
+  document.getElementById("r2c9").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 3 //
 
 function r3c7 () {
   document.getElementById("r3c7").innerHTML = turn;
@@ -169,4 +171,328 @@ function r3c9 () {
   update_turn();
 }
 
-// -----/Row 3----- //
+//----- /CELL 3 -----//
+//----- CELL 4 -----//
+
+// ROW 4 //
+
+function r4c1 () {
+  document.getElementById("r4c1").innerHTML = turn;
+  update_turn();
+}
+
+function r4c2 () {
+  document.getElementById("r4c2").innerHTML = turn;
+  update_turn();
+}
+
+function r4c3 () {
+  document.getElementById("r4c3").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 5 //
+
+function r5c1 () {
+  document.getElementById("r5c1").innerHTML = turn;
+  update_turn();
+}
+
+function r5c2 () {
+  document.getElementById("r5c2").innerHTML = turn;
+  update_turn();
+}
+
+function r5c3 () {
+  document.getElementById("r5c3").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 6 //
+
+function r6c1 () {
+  document.getElementById("r6c1").innerHTML = turn;
+  update_turn();
+}
+
+function r6c2 () {
+  document.getElementById("r6c2").innerHTML = turn;
+  update_turn();
+}
+
+function r6c3 () {
+  document.getElementById("r6c3").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 4 -----//
+//----- CELL 5 -----//
+
+// ROW 4 //
+
+function r4c4 () {
+  document.getElementById("r4c4").innerHTML = turn;
+  update_turn();
+}
+
+function r4c5 () {
+  document.getElementById("r4c5").innerHTML = turn;
+  update_turn();
+}
+
+function r4c6 () {
+  document.getElementById("r4c6").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 5 //
+
+function r5c4 () {
+  document.getElementById("r5c4").innerHTML = turn;
+  update_turn();
+}
+
+function r5c5 () {
+  document.getElementById("r5c5").innerHTML = turn;
+  update_turn();
+}
+
+function r5c6 () {
+  document.getElementById("r5c6").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 6 //
+
+function r6c4 () {
+  document.getElementById("r6c4").innerHTML = turn;
+  update_turn();
+}
+
+function r6c5 () {
+  document.getElementById("r6c5").innerHTML = turn;
+  update_turn();
+}
+
+function r6c6 () {
+  document.getElementById("r6c6").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 5 -----// 
+//----- CELL 6 -----//
+
+// ROW 4 //
+
+function r4c7 () {
+  document.getElementById("r4c7").innerHTML = turn;
+  update_turn();
+}
+
+function r4c8 () {
+  document.getElementById("r4c8").innerHTML = turn;
+  update_turn();
+}
+
+function r4c9 () {
+  document.getElementById("r4c9").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 5 //
+
+function r5c7 () {
+  document.getElementById("r5c7").innerHTML = turn;
+  update_turn();
+}
+
+function r5c8 () {
+  document.getElementById("r5c8").innerHTML = turn;
+  update_turn();
+}
+
+function r5c9 () {
+  document.getElementById("r5c9").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 6 //
+
+function r6c7 () {
+  document.getElementById("r6c7").innerHTML = turn;
+  update_turn();
+}
+
+function r6c8 () {
+  document.getElementById("r6c8").innerHTML = turn;
+  update_turn();
+}
+
+function r6c9 () {
+  document.getElementById("r6c9").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 6 -----//
+//----- CELL 7 -----//
+
+// ROW 7 //
+
+function r7c1 () {
+  document.getElementById("r7c1").innerHTML = turn;
+  update_turn();
+}
+
+function r7c2 () {
+  document.getElementById("r7c2").innerHTML = turn;
+  update_turn();
+}
+
+function r7c3 () {
+  document.getElementById("r7c3").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 8 //
+
+function r8c1 () {
+  document.getElementById("r8c1").innerHTML = turn;
+  update_turn();
+}
+
+function r8c2 () {
+  document.getElementById("r8c2").innerHTML = turn;
+  update_turn();
+}
+
+function r8c3 () {
+  document.getElementById("r8c3").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 9 //
+
+function r9c1 () {
+  document.getElementById("r9c1").innerHTML = turn;
+  update_turn();
+}
+
+function r9c2 () {
+  document.getElementById("r9c2").innerHTML = turn;
+  update_turn();
+}
+
+function r9c3 () {
+  document.getElementById("r9c3").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 7 -----//
+//----- CELL 8 -----//
+
+// ROW 7 //
+
+function r7c4 () {
+  document.getElementById("r7c4").innerHTML = turn;
+  update_turn();
+}
+
+function r7c5 () {
+  document.getElementById("r7c5").innerHTML = turn;
+  update_turn();
+}
+
+function r7c6 () {
+  document.getElementById("r7c6").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 8 //
+
+function r8c4 () {
+  document.getElementById("r8c4").innerHTML = turn;
+  update_turn();
+}
+
+function r8c5 () {
+  document.getElementById("r8c5").innerHTML = turn;
+  update_turn();
+}
+
+function r8c6 () {
+  document.getElementById("r8c6").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 9 //
+
+function r9c4 () {
+  document.getElementById("r9c4").innerHTML = turn;
+  update_turn();
+}
+
+function r9c5 () {
+  document.getElementById("r9c5").innerHTML = turn;
+  update_turn();
+}
+
+function r9c6 () {
+  document.getElementById("r9c6").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 8 -----//
+//----- CELL 9 -----//
+
+// ROW 7 //
+
+function r7c7 () {
+  document.getElementById("r7c7").innerHTML = turn;
+  update_turn();
+}
+
+function r7c8 () {
+  document.getElementById("r7c8").innerHTML = turn;
+  update_turn();
+}
+
+function r7c9 () {
+  document.getElementById("r7c9").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 8 //
+
+function r8c7 () {
+  document.getElementById("r8c7").innerHTML = turn;
+  update_turn();
+}
+
+function r8c8 () {
+  document.getElementById("r8c8").innerHTML = turn;
+  update_turn();
+}
+
+function r8c9 () {
+  document.getElementById("r8c9").innerHTML = turn;
+  update_turn();
+}
+
+// ROW 9 //
+
+function r9c7 () {
+  document.getElementById("r9c7").innerHTML = turn;
+  update_turn();
+}
+
+function r9c8 () {
+  document.getElementById("r9c8").innerHTML = turn;
+  update_turn();
+}
+
+function r9c9 () {
+  document.getElementById("r9c9").innerHTML = turn;
+  update_turn();
+}
+
+//----- /CELL 9 -----//
