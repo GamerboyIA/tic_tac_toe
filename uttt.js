f2b7cbf66fee0b2e3ddca4fb79f610407787b529
isaac akins <isaac@ideadrop.com>
2024-11-01 10:57:41 -0400

    uttt upgrades

--- uttt.js
+++ uttt.js
@@ -1,7 +1,8 @@
 var turn = 'X';
 var lastSquare;
-
+var cellOfPlay;
 function update_turn() {
+  firstMove = false; 
   if (turn == 'X') {
     turn = 'O';
   } else {
@@ -25,9 +26,18 @@ undoable = false;
 }
 
 var lastSquare;
-
+var cellOfPlay;
+var firstMove = true; 
 function takeTurn(squareId) {
+   console.log("making a move!!!"); 
 
+	// Check if we are working in the correct cell 
+	if (!firstMove)  {
+		valid_cell_row = Math.floor(Number(lastSquare[1])/3);
+		valid_cell_col = Math.floor(Number(lastSquare[3])/3);
+		console.log([valid_cell_row, valid_cell_col]);
+	}
+   
    console.log('It is ' + turn + ' turn. ' + squareId + ' chosen');
    lastSquare = squareId;
    
@@ -41,3 +51,22 @@ function takeTurn(squareId) {
   square.innerHTML = turn;
   update_turn();
 }
+
+
+// 1) r1c1 - r3c3
+
+// 2) r1c4 - r3c6
+
+// 3) r1c7 - r3c9
+
+// 4) r4c1 - r6c3
+
+// 5) r4c4 - r6c6
+
+// 6) r4c7 - r6c9
+
+// 7) r7c1 - r9c3
+
+// 8) r7c4 - r9c6
+
+// 9) r7c7 - r9c9
