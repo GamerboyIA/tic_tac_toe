3b971874e736533fea594c49f19cbb12dcc3c284
Isaac Akins <isaac@ideadrop.comx>
2025-04-28 21:03:25 -0400

    refactor

--- uttt.js
+++ uttt.js
@@ -8,6 +8,18 @@
 var turn = 'X';
 var turns = Array();
 
+var boardData = [
+	 [getCellData(), getCellData(), getCellData()],
+	 [getCellData(), getCellData(), getCellData()],
+   [getCellData(), getCellData(), getCellData()]
+];
+
+var cellMap = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9]
+];
+
 function update_turn() {
   firstMove = false; 
   if (turn == 'X') {
@@ -29,6 +41,15 @@ function update_turn() {
 	}
    
 }
+
+function getCellData() {
+  return [
+	 [null, null, null ],
+	 [null, null, null ],
+	 [null, null, null ]	
+];
+}
+
 
 function undo() {
 	if (turns.length == 0) {
@@ -54,111 +75,22 @@ if (document.getElementById("lastMove").innerHTML == "undefined") {
 // Ranges for each cell
 function getCell(squareId) {
 	var row = Number(squareId[1]);
-	var col = Number(squareId[3]);
+	var col = Number(squareId[2]);
 	
-	if (row < 4 && col < 4) {
-		return 1;
-	}
-
-	if (row < 4 && col > 3 && col < 7) {
-		return 2;
-	}
-
-	if (row < 4 && col > 6) {
-		return 3;
-	}
-	
-	if (row > 3 && row < 7 && col < 4) {
-		return 4;
-	}
-
-	if (row > 3 && row < 7 && col > 3 && col < 7) {
-		return 5;
-	}
-	
-	if (row > 3 && row < 7 && col > 6) {
-		return 6;
-	}
-
-	if (row > 6 && col < 4) {
-		return 7;
-	}
-
-	if (row > 6 && col > 3 && col < 7) {
-		return 8;
-	}
-	
-	if (row > 6 && col > 6) {
-		return 9;
-	}	
+	return cellMap[row][col];
 }
+
 // Which cell will be next based on last played square
 function getNextCell(squareId) {
-	var row = Number(squareId[1]);
-	var col = Number(squareId[3]);
-
-	if (row == 1 || row == 4 || row == 7) {
-		cellRow = 1;
-	}	
-
-	if (row == 2 || row == 5 || row == 8) {
-		cellRow = 2;
-	}	
-
-	if (row == 3 || row == 6 || row == 9) {
-		cellRow = 3;
-	}	
-
-	if (col == 1 || col == 4 || col == 7) {
-		cellCol = 1;
-	}	
-
-	if (col == 2 || col == 5 || col == 8) {
-		cellCol = 2;
-	}	
-
-	if (col == 3 || col == 6 || col == 9) {
-		cellCol = 3;
-	}	
-
-	if (cellRow == 1 && cellCol == 1) {
-		return 1;
-	}
-
-	if (cellRow == 1 && cellCol == 2) {
-		return 2;
-	}
-
-	if (cellRow == 1 && cellCol == 3) {
-		return 3;
-	}
+	var row = Number(squareId[3]);
+	var col = Number(squareId[4]);
 	
-	if (cellRow == 2 && cellCol == 1) {
-		return 4;
-	}
+	return cellMap[row][col];
 
-	if (cellRow == 2 && cellCol == 2) {
-		return 5;
-	}
-
-	if (cellRow == 2 && cellCol == 3) {
-		return 6;
-	}
-
-	if (cellRow == 3 && cellCol == 1) {
-		return 7;
-	}
-
-	if (cellRow == 3 && cellCol == 2) {
-		return 8;
-	}
-
-	if (cellRow == 3 && cellCol == 3) {
-		return 9;
-	}
 }
 
-
+//              0123
+// Square id is RXCY
 
 function takeTurn(squareId) {
    console.log('It is ' + turn + ' turn. ' + squareId + ' chosen');
@@ -166,7 +98,7 @@ function takeTurn(squareId) {
   // document.getElementById("lastMove").innerHMTL == squareId;  
    var square = document.getElementById(squareId);
    if (square.innerHTML.length > 0) {
-   		console.log('Nope');
+   	  console.log('Nope');
       return;
    }
 
@@ -190,17 +122,49 @@ function takeTurn(squareId) {
    console.log('you selected cell ' + getCell(squareId));
    console.log('next cell is ' + getNextCell(squareId));
 
-if (turns.length !== 0) {
+   if (turns.length !== 0) {
      document.getElementById("lastMove").innerHTML = turns[turns.length -1];
-
-} else { 
-
+    } else { 
      document.getElementById("lastMove").innerHTML == "---";
 
+   }
+
+   square.innerHTML = turn;
+   update_turn();
 }
 
+function getWinner(cell) {
+	// check for horizontal win
+	for (let r=0; r<3; r++) {
+		if (cell[r][0] === cell[r][1] && cell[r][0] === cell[r][2] && cell[r][0] !== null) {
+      console.log('H');
+			return cell[r][0];
+		}
+	}
+
+  // check for vertical win
+	for (let c=0; c<3; c++) {
+    console.log(cell[c]);
+		if (cell[0][c] === cell[1][c] && cell[0][c] === cell[2][c] && cell[0][c] !== null) {
+      console.log('V');
+			return cell[0][c];
+		}
+	}
+  
+  // check top left to bottom right diagaol
+  if (cell[0][0] === cell[1][1] && cell[0][0] === cell[2][2] && cell[0][0] !== null) {
+      console.log('D1');
+    return cell[0][0];
+  }
+  
+    // check bottom left to top right diagaol
+  if (cell[2][0] === cell[1][1] && cell[2][0] === cell[0][2] && cell[2][0] !== null) {
+      console.log('D2');
+    return cell[2][0];
+  }
+  
+  return null;
+ }
 
 
-  square.innerHTML = turn;
-  update_turn();
-}
+
